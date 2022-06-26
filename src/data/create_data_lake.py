2347ac3da2813e6681c783ae75038908357b6f93
macanoso <macanoso@unal.edu.co>
2022-06-26 16:50:43 -0500

    Data_Lake

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,10 +21,34 @@ def create_data_lake():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import os
+    from pathlib import Path
+
+    parent_directory = Path(__file__).parent.parent
+    data_lake = os.mkdir(os.path.join(parent_directory, "data_lake"))
+    directory = [
+        "data_lake/landing",
+        "data_lake/raw",
+        "data_lake/cleansed",
+        "data_lake/business",
+    ]
+
+    for d in directory:
+        os.mkdir(os.path.join(parent_directory, d))
+
+    dir_business = [
+        "data_lake/business/reports",
+        "data_lake/business/reports/figures",
+        "data_lake/business/features",
+        "data_lake/business/forecasts",
+    ]
+
+    for d in dir_business:
+        os.mkdir(os.path.join(parent_directory, d))
 
 
 if __name__ == "__main__":
     import doctest
 
+    create_data_lake()
     doctest.testmod()
