4f00df480c6a5b24c4c0d8cfc479852e6432a620
macanoso <macanoso@unal.edu.co>
2022-06-26 19:14:36 -0500

    ingest_data

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,10 +13,36 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    import requests
+    from bs4 import BeautifulSoup
+    import re
+
+    url = "https://github.com/jdvelasq/datalabs/tree/master/datasets/precio_bolsa_nacional/xls"
+
+    r = requests.get(url)
+    html_doc = r.text
+    soup = BeautifulSoup(html_doc, features="html.parser")
+    a_tags = soup.find_all("a")
+    urls_xlsx = [
+        "https://raw.githubusercontent.com" + re.sub("/blob", "", link.get("href"))
+        for link in a_tags
+        if ".xlsx" in link.get("href")
+    ]
+
+    for i in urls_xlsx:
+        """save the url in the landing folder"""
+        file = requests.get(i)
+        open("data_lake/landing/{}".format(i.split("/")[-1]), "wb").write(file.content)
+
+    for i in range(2016, 2018):
+        url_xls = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true"
+        file = requests.get(url_xls, allow_redirects=True)
+        open("data_lake/landing/{}.xls".format(i), "wb").write(file.content)
 
 
 if __name__ == "__main__":
     import doctest
 
+    ingest_data()
     doctest.testmod()
