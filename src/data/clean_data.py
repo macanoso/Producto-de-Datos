8aae92a5a718d89e05830c4d39361a5e65a9c6f2
macanoso <macanoso@unal.edu.co>
2022-06-27 16:11:58 -0500

    Clean

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -12,9 +12,42 @@ def clean_data():
 
 
     """
+    import pandas as pd
+    import os
+
+    path = "data_lake/raw"
+    FichList = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+    cleansed_df = []
+
+    for f in FichList:
+        df_read = pd.read_csv(
+            "data_lake/raw/{}".format(f),
+        )
+        cleansed_df.append(df_read)
+
+    frame = pd.concat(cleansed_df, axis=0, ignore_index=True)
+    old_columns = frame.columns
+    new_columns = ["fecha"] + ["{0:0=2d}".format(int(i)) for i in old_columns[1:]]
+    frame.columns = new_columns
+
+    frame["fecha"] = pd.to_datetime(frame["fecha"], format="%Y-%m-%d")
+    unpivoted_table = frame.melt(
+        id_vars=["fecha"],
+        value_vars=new_columns[1:],
+        var_name="hora",
+        value_name="precio",
+    )
+    unpivoted_table["precio"] = unpivoted_table["precio"].fillna(
+        unpivoted_table.groupby("fecha")["precio"].transform("mean")
+    )
+    unpivoted_table.to_csv(
+        "data_lake/cleansed/precios-horarios.csv", encoding="utf-8", index=False
+    )
 
 
 if __name__ == "__main__":
     import doctest
 
+    clean_data()
     doctest.testmod()
