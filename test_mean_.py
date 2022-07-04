ddbb43a63195213e63644b23c84c3892002025fb
macanoso <macanoso@unal.edu.co>
2022-07-04 00:13:53 -0500

    test

--- test_mean_.py
+++ test_mean_.py
@@ -1,6 +1,7 @@
 """
 Esta funciona testea el promedio de los precios de los productos
 """
+# pylint: disable=unused-import
 
 import pytest
 import pandas as pd
@@ -9,11 +10,17 @@ import pandas as pd
 
 
 def import_data():
+    """
+    Importa los datos de la tabla unica de precios horarios.
+    """
     df_test = pd.read_csv("data_lake/business/precios-diarios.csv")
     return df_test
 
 
 def test_data_():
+    """
+    Esta funciona testea el promedio de los precios de los productos
+    """
     df_test = import_data()
     expect1 = 118.82415731973808
     assert1 = df_test["precio"].mean()
